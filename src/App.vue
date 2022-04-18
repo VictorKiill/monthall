ba9c3fffd829da9fa43e07de63a470c99c3df868
VictorKiill <victorkiill@gmail.com>
2022-04-18 20:49:03 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Problema de Mount Hall</h1>
+  <div class="form">
+    <div v-if="!started">
+      <label for="doorsAmount">Quantas portas? </label>
+      <input type="text" id="doorsAmount" size="3" v-model.number="doorsAmount">
+    </div>
+    <div v-if="!started">
+      <label for="selectedDoor">Qual a porta premiada? </label>
+      <input type="text" id="selectedDoor" size="3" v-model.number="selectedDoor">
+    </div>
+    <button v-if="!started" @click="started = true">Começar</button>
+    <button v-if="started" @click="started = false">Reiniciar</button>
+  </div>
+  <div class="doors" v-if="started">
+    <div v-for="i in doorsAmount" :key="i">
+      <the-door :hasGift="i === selectedDoor" :number="i" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TheDoor from './components/TheDoor.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { TheDoor },
+  data() {
+    return {
+      started: false,
+      doorsAmount: 3,
+      selectedDoor: null
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  * {
+    box-sizing: border-box;
+    font-family: 'Montserrat', sans-serif;
+  }
+
+  body {
+    color: #fff;
+    background: linear-gradient(90deg, rgba(9,131,215,1) 0%, rgba(40,245,208,1) 100%);;
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+  }
+
+.form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 40px;
+}
+
+.form, .form input, .form button {
+  margin-bottom: 10px;
+  font-size: 2rem;
+}
+
+.doors {
+  align-self: stretch;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
 }
 </style>
---
+++ src/components/TheDoor.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="door-area">
+        <div class="door-frame" :class="{ selected: selected && !open }">
+            <the-gift v-if="open && hasGift"/>
+        </div>
+        <div class="door" @click="selected = !selected" :class="{ open }">
+            <div class="number" :class="{ selected }">{{ number }}</div>
+            <div class="knob" :class="{ selected }" @click.stop="open = true"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+import TheGift from "./TheGift.vue";
+
+export default {
+  name: "TheDoor",
+  components: { TheGift },
+  props: {
+      number: {},
+      hasGift: { type: Boolean }
+  },
+  data() {
+      return {
+          open: false,
+          selected: false
+      }
+  },
+};
+</script>
+
+<style>
+:root {
+    --door-border: 5px solid brown;
+    --selected-border: 5px solid yellow;
+}
+
+.door-area {
+    position: relative;
+    width: 200px;
+    height: 310px;
+    border-bottom: 10px solid #aaa;
+    margin-bottom: 20px;
+    font-size: 3rem;
+
+    display: flex;
+    justify-content: center;
+}
+
+.door-frame {
+    position: absolute;
+    height: 300px;
+    width: 180px;
+
+    border: var(--door-border);
+    border-bottom: none;
+
+    display: flex;
+    justify-content: center;
+    align-items: flex-end;
+}
+
+.door {
+    position: absolute;
+    top: 5px;
+    height: 295px;
+    width: 170px;
+    background-color: chocolate;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+}
+
+.door .knob {
+    height: 20px;
+    width: 20px;
+    border-radius: 10px;
+    background-color: brown;
+    align-self: flex-start;
+    margin-top: 60px;
+}
+
+.door-frame.selected {
+    border: var(--selected-border);
+    border-bottom: none;
+}
+
+.door .number.selected {
+    color: yellow;
+}
+
+.door .knob.selected {
+    background-color: yellow;
+}
+
+.door.open {
+    background-color: #0007;
+}
+
+.door.open .number {
+    display: none;
+}
+
+.door.open .knob {
+    display: none;
+}
+</style>
